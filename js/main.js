9afe4bba2042779c60257d2b4104d85e57e64d3c
Lutsic <125072455+Lutsic@users.noreply.github.com>
2026-03-21 09:26:27 +0100

    Complete website redesign
    Added file with js code

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const search = document.getElementById("search");
+const results = document.getElementById("results");
+const statusEl = document.getElementById("status");
+
+function openTab(index) {
+    let tabs = document.querySelectorAll('.content');
+    tabs.forEach(tab => tab.classList.remove('active'));
+    tabs[index].classList.add('active');
+}
+
+function loadScript(url) {
+  return new Promise((resolve, reject) => {
+    const s = document.createElement("script");
+    s.src = url;
+    s.onload = resolve;
+    s.onerror = () => reject(new Error("Error during loading script"));
+    document.body.appendChild(s);
+  });
+}
+
+loadScript("https://oultqkpsmqszpiqzahih.supabase.co/storage/v1/object/public/files/data.js")
+  .then(() => {
+    statusEl.textContent = "All data loaded correctly";
+  })
+  .catch(err => {
+    console.error(err);
+    statusEl.textContent = "Error during data loading: " + err.message;
+  });
+
+search.oninput = () => {
+  const q = search.value.toLowerCase();
+  results.innerHTML = "";
+
+  if (typeof DATA === "undefined" || !q) return;
+
+  let count = 0;
+
+  for (const name in DATA) {
+    if (name.toLowerCase().includes(q)) {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+
+      a.textContent = name;
+      a.href = DATA[name];
+      a.target = "_blank";
+
+      li.appendChild(a);
+      results.appendChild(li);
+
+      if (++count >= 500) break;
+    }
+  }
+};
